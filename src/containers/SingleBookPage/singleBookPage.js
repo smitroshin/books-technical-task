38ff3bbdb409dcabb388731de7b9db8b20c29f16
Serghei Mitroshin <serghei.mitroshin@gmail.com>
2021-06-01 12:28:45 +0300

    Handle wrong ID on SingleBookPage

--- src/containers/SingleBookPage/singleBookPage.js
+++ src/containers/SingleBookPage/singleBookPage.js
@@ -1,21 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { Card, Image, Descriptions, Tag, Spin } from 'antd';
+import { Card, Image, Descriptions, Tag, Spin, Result, Button } from 'antd';
 import API from '../../utils/appAPI';
+import history from '../../utils/history';
 
 import './singleBookPage.scss';
 
 const SingleBookPage = () => {
   const { bookId } = useParams();
-  const [bookInfo, setBookInfo] = useState();
+  const [error, setError] = useState({ status: undefined, message: '' });
+  const [bookInfo, setBookInfo] = useState({ isLoaded: false, data: {} });
 
   useEffect(() => {
     if (bookId)
-      API.books.getSingle(bookId).then((res) => setBookInfo(res.data));
+      API.books
+        .getSingle(bookId)
+        .then((res) =>
+          setBookInfo((prevState) => ({
+            ...prevState,
+            isLoaded: true,
+            data: res.data,
+          })),
+        )
+        .catch((err) => {
+          if (err.response.data.error)
+            setError((prevState) => ({
+              ...prevState,
+              status:
+                [404, 403, 500].find(
+                  (itm) => itm === err.response.data.error.code,
+                ) || 'error',
+              message: err.response.data.error.message,
+            }));
+          else
+            setError((prevState) => ({
+              ...prevState,
+              status: 'error',
+              message: 'Something went wrong',
+            }));
+        });
   }, [bookId]);
 
-  if (!bookInfo)
+  const handleGoBack = useCallback(() => history.goBack(), []);
+
+  if (error.status)
+    return (
+      <Result
+        status={error.status}
+        title={error.status}
+        subTitle={error.message}
+        extra={
+          <Button type="primary" onClick={handleGoBack}>
+            Go back
+          </Button>
+        }
+      />
+    );
+
+  if (!bookInfo.isLoaded)
     return (
       <div
         className="w-100 d-flex justify-content-center align-items-center"
@@ -28,48 +71,50 @@ const SingleBookPage = () => {
   return (
     <div className="single-book container-fluid mx-auto">
       <Helmet>
-        <title>{bookInfo.volumeInfo.title} | Search Books</title>
+        <title>{bookInfo.data.volumeInfo.title} | Search Books</title>
       </Helmet>
       <Card>
-        <h2 className="d-inline-block me-2">{bookInfo.volumeInfo.title}</h2>
-        {bookInfo.volumeInfo.subtitle && (
+        <h2 className="d-inline-block me-2">
+          {bookInfo.data.volumeInfo.title}
+        </h2>
+        {bookInfo.data.volumeInfo.subtitle && (
           <h3 className="d-inline-block text-secondary">
-            {bookInfo.volumeInfo.subtitle}
+            {bookInfo.data.volumeInfo.subtitle}
           </h3>
         )}
         <div className="row justify-content-between">
           <div className="col-12 col-md-auto">
             <Image
               width={200}
-              src={bookInfo.volumeInfo?.imageLinks?.thumbnail}
+              src={bookInfo.data.volumeInfo?.imageLinks?.thumbnail}
             />
           </div>
           <div className="col">
             <Descriptions title="Details" column={1}>
               <Descriptions.Item label="Language">
-                {bookInfo.volumeInfo.language}
+                {bookInfo.data.volumeInfo.language}
               </Descriptions.Item>
               <Descriptions.Item label="Author">
-                {bookInfo.volumeInfo.authors.join(', ')}
+                {bookInfo.data.volumeInfo.authors.join(', ')}
               </Descriptions.Item>
               <Descriptions.Item label="Publisher">
-                {bookInfo.volumeInfo.publisher}
+                {bookInfo.data.volumeInfo.publisher}
               </Descriptions.Item>
               <Descriptions.Item label="Print type">
-                {bookInfo.volumeInfo.printType}
+                {bookInfo.data.volumeInfo.printType}
               </Descriptions.Item>
               <Descriptions.Item label="Published date">
-                {bookInfo.volumeInfo.publishedDate}
+                {bookInfo.data.volumeInfo.publishedDate}
               </Descriptions.Item>
               <Descriptions.Item label="Page count">
-                {bookInfo.volumeInfo.pageCount}
+                {bookInfo.data.volumeInfo.pageCount}
               </Descriptions.Item>
               <Descriptions.Item label="Printed page count">
-                {bookInfo.volumeInfo.printedPageCount}
+                {bookInfo.data.volumeInfo.printedPageCount}
               </Descriptions.Item>
               <Descriptions.Item label="Categories">
                 <div>
-                  {bookInfo.volumeInfo.categories?.map((itm) => (
+                  {bookInfo.data.volumeInfo.categories?.map((itm) => (
                     <Tag key={itm} className="mb-2">
                       {itm}
                     </Tag>
@@ -78,7 +123,7 @@ const SingleBookPage = () => {
               </Descriptions.Item>
               <Descriptions.Item label="Preview">
                 <a
-                  href={`${bookInfo.volumeInfo.previewLink}&printsec=frontcover`}
+                  href={`${bookInfo.data.volumeInfo.previewLink}&printsec=frontcover`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -89,7 +134,7 @@ const SingleBookPage = () => {
               <Descriptions.Item>
                 <div
                   dangerouslySetInnerHTML={{
-                    __html: bookInfo.volumeInfo.description,
+                    __html: bookInfo.data.volumeInfo.description,
                   }}
                 />
               </Descriptions.Item>
